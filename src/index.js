dfc09e7b09bc4873b21a5207d309a4de4d2d5909
Eliasz Rubcow <rubtsovi.90@gmail.com>
2019-04-14 16:00:44 +0200

    just another commit

--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ import { jqueryIcon } from './assets/icons/jquery-icon';
 import { stagger, styler, spring } from 'popmotion';
 
 const personalInfoPart = require('./partials/personal-info-bar.hbs');
-    const personalInfoData = require('./personal-data.json');
-    
-    const personalDataSection = document.getElementById('personal-info-bar');
-    personalDataSection.innerHTML = personalInfoPart(personalInfoData);
-    
-    library.add(faHome, faAddressBook, faShareAlt, faCss3, faHtml5, faJs, faPhp, jqueryIcon);
-    dom.watch();
-    
-    const stylers = Array.from(document.querySelectorAll('.skill-pointer')).map(styler);
-    let animations = Array(stylers.length).fill(spring({to: '100%', stiffness: 15, damping: 5}));
-    
-    
-    stagger(animations, 300)
-        .start((pointerProps) => pointerProps.forEach((pointerProp, i) => stylers[i].set('left', pointerProp)));
+const infoCard = require('./partials/right-info-card.hbs');
+const personalInfoData = require('./personal-data.json');
+
+const personalDataSection = document.getElementById('personal-info-bar');
+personalDataSection.innerHTML = personalInfoPart(personalInfoData);
+document.getElementById('education-block').innerHTML = infoCard();
+document.getElementById('professional-experience-block').innerHTML = infoCard();
+
+library.add(faHome, faAddressBook, faShareAlt, faCss3, faHtml5, faJs, faPhp, jqueryIcon);
+dom.watch();
+
+const stylers = Array.from(document.querySelectorAll('.skill-pointer')).map(styler);
+let animations = Array(stylers.length).fill(spring({ to: '100%', stiffness: 15, damping: 5 }));
+
+
+stagger(animations, 300)
+    .start((pointerProps) => pointerProps.forEach((pointerProp, i) => stylers[i].set('left', pointerProp)));
