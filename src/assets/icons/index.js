bd712b28facae5aa74826ec65c042702a0a5ac30
rubtsovi <rubtsovi.90@gmail.com>
2019-07-04 22:30:46 +0200

    cv without portfolio is ready

---
+++ src/assets/icons/index.js
@@ -0,0 +1,9 @@
+// import { jqueryIcon } from "./jquery-icon";
+// import { photoshopIcon } from './photoshop-icon';
+// import { webpackIcon } from './webpack-icon';
+
+export { jqueryIcon } from './jquery-icon';
+export { webpackIcon } from './webpack-icon';
+export { photoshopIcon } from './photoshop-icon';
+export { typescriptIcon } from './typescript-icon';
+export { mysqlIcon } from './mysql-icon';
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import style from './assets/styles/style.scss';
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faHome, faAddressBook, faShareAlt } from '@fortawesome/free-solid-svg-icons';
-import { faHtml5, faCss3, faJs, faPhp } from '@fortawesome/free-brands-svg-icons';
-import { jqueryIcon } from './assets/icons/jquery-icon';
+import { faHome, faAddressBook, faShareAlt, faGraduationCap, faBriefcase, faCalendarAlt, faLaptopCode, faPaperclip } from '@fortawesome/free-solid-svg-icons';
+import { faHtml5, faCss3, faJs, faPhp, faSymfony, faAngular, faYarn, faBootstrap, faGitAlt, faSass } from '@fortawesome/free-brands-svg-icons';
+import { jqueryIcon, webpackIcon, photoshopIcon, typescriptIcon, mysqlIcon } from './assets/icons';
 
 import { stagger, styler, spring } from 'popmotion';
 
 const personalInfoPart = require('./partials/personal-info-bar.hbs');
 const infoCard = require('./partials/right-info-card.hbs');
+const miscellaneousCard = require('./partials/miscellaneous-info-card.hbs');
 const personalInfoData = require('./personal-data.json');
 
 const personalDataSection = document.getElementById('personal-info-bar');
 personalDataSection.innerHTML = personalInfoPart(personalInfoData);
-document.getElementById('education-block').innerHTML = infoCard();
-document.getElementById('professional-experience-block').innerHTML = infoCard();
+document.getElementById('education-block').innerHTML = infoCard(require('./education-data.json'));
+document.getElementById('professional-experience-block').innerHTML = infoCard(require('./professional-experience.json'));
+document.getElementById('misceallenous-block').innerHTML = miscellaneousCard(require('./miscellaneous.json'));
+// document.getElementById('professional-experience-block').innerHTML = infoCard();
 
-library.add(faHome, faAddressBook, faShareAlt, faCss3, faHtml5, faJs, faPhp, jqueryIcon);
+library.add(faHome, faAddressBook, faShareAlt, faCss3, faHtml5, faJs, faPhp, jqueryIcon, faGraduationCap, faBriefcase, faCalendarAlt, faLaptopCode, faPaperclip, faSymfony, faAngular, faYarn, webpackIcon, faBootstrap, faGitAlt, faSass, photoshopIcon, typescriptIcon, mysqlIcon);
 dom.watch();
 
 const stylers = Array.from(document.querySelectorAll('.skill-pointer')).map(styler);
